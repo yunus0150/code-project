ba4f071b247193b8c9b8ad4c026252c156369a93
yunus0150 <yunusjani403@gmail.com>
2026-01-31 12:16:42 +0530

    Add files via upload

---
+++ bill.py
@@ -0,0 +1,6 @@
+Hostel= int(input("Enter Hostel fee :  " ,))
+Course= int(input("Enter Course fee : " ,))
+Expenditure= int(input("Enter montly Expenditure" ,))
+total=(Hostel+Course+Expenditure)
+print(total)
+
