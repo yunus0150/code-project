fdc93e5f789c04aa7f0c6ae3818c6f8e79976e57
SK YUNUS JANI <yunusjani403@gmail.com>
2026-02-18 17:05:17 +0530

    Add files via upload

---
+++ #case convertions.py
@@ -0,0 +1,10 @@
+#case convertions
+x="yunus jani is a FrOnT End DevelOPer"
+x.lower()
+print(x.lower())
+x.upper()
+print(x.upper())
+x.capitalize()
+print(x.capitalize())
+x.title()
+print(x.title())
