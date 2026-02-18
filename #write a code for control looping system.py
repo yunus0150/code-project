d4fc50cbe13fd65bc26f12d9ea51ae1568975e6d
SK YUNUS JANI <yunusjani403@gmail.com>
2026-02-18 16:55:52 +0530

    Add files via upload

---
+++ #write a code for control looping system.py
@@ -0,0 +1,9 @@
+#write a code for control looping system
+for i in range(1,101):
+    if i==95:
+        continue
+        break
+    else:
+        print(i,".Codegnan")
+else:
+    print("Completed")    
