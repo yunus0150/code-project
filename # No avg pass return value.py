17fd8a1552dcd1108b1565ec81bcc59308f13803
SK YUNUS JANI <yunusjani403@gmail.com>
2026-02-28 21:57:59 +0530

    Add files via upload

---
+++ # No avg pass return value.py
@@ -0,0 +1,7 @@
+# No avg pass  return value
+def powered():
+    a=int(input("Enter the value of a :"))
+    b=int(input("Enter the value of b:"))
+    return a**b
+result = powered()            
+print(result)
