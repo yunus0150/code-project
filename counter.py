0872be723f22ebfe42f45a940fb652b3c42442fe
SK YUNUS JANI <yunusjani403@gmail.com>
2026-02-28 21:43:29 +0530

    Add files via upload

---
+++ counter.py
@@ -0,0 +1,13 @@
+def Akhil(n):
+    c=0
+    for i in n:
+        c+=1
+    return(c)
+Akhil([1,2,3,4,5,6,7,8,9,])
+
+def Great_Mava(n):
+    m=0
+    for i in n:
+        if i>m:
+            m=i
+    return m         
---
+++ test.py
@@ -0,0 +1,8 @@
+import counter
+#from counter import Akhil,Great_Mava
+from counter import*
+
+a=Akhil([1,2,3,4,5,6,7,8,9])
+print(a)
+
+print(counter.Great_Mava([10,14,12,7,7,8]))
