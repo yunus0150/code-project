13b17e280ca05af0e50dec8c6382ada88aca376c
SK YUNUS JANI <yunusjani403@gmail.com>
2026-02-28 21:52:47 +0530

    Add files via upload

---
+++ #write a code to print even  or odd numbers in loop condition.py
@@ -0,0 +1,8 @@
+#write a code to print even numbers in loop condition
+for i in range(1,10,1):
+    if i%2==0:
+        print(i)
+#write a code to print odd numbers in loop condition
+for i in range(1,10,1):
+    if i%2==1:
+        print(i)         
