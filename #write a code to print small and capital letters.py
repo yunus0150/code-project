f5d5b776fa0aff77722174af90f8aeab5f1c89b6
SK YUNUS JANI <yunusjani403@gmail.com>
2026-02-28 22:07:22 +0530

    Add files via upload

---
+++ #write a code to print small and capital letters.py
@@ -0,0 +1,7 @@
+#write a code to print small letters
+for i in range(97,123):
+    print(chr(i),end=" ")
+print()    
+#write a code to print capital letters
+for i in range(65,91):
+    print(chr(i),end=" ")    
